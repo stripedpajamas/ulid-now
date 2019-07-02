e5800fb2a441ea4d7b46211c91abb6f29192f4a2
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-07-02 14:47:45 -0700

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const { ulid } = require('ulid')
 
-document.getElementById('refresh').onclick = getUlid
+const refreshBtn = document.getElementById('refresh')
+const copyBtn = document.getElementById('copy')
+
+refreshBtn.onclick = getUlid
+copyBtn.onclick = copyUlid
 
 function getUlid () {
   document.getElementById('ulid-output').textContent = ulid()
+}
+
+function copyUlid () {
+  const currentUlid = document.getElementById('ulid-output').textContent
+  const tempInput = document.createElement('input')
+  tempInput.value = currentUlid
+  document.body.appendChild(tempInput)
+  tempInput.select()
+  document.execCommand('copy')
+  tempInput.remove()
 }
 
 getUlid()
